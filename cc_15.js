85eb99c54133a85605e17f1cf9b3c8b7a8b0a97b
Angel75Git <aduboncenteno@usf.edu>
2025-03-16 19:47:59 -0400

    Task 2 - Adding Risk Items

--- cc_15.js
+++ cc_15.js
@@ -2,4 +2,54 @@
 //Selecting container 
 const riskContainer = document.getElementById("riskDashboard");
  console.log("Risk Dashboard Loaded");
+
+ //Task 2
+ function addRiskItem(riskName, riskLevel, department) {
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("risk-card");
+
+//Getting the name of the risk on the card
+    const itemName = document.createElement("h3");
+    itemName.textContent = riskName;
+
+//Getting the level of the risk on the card
+const itemLevel = document.createElement("span");
+itemLevel.textContent = `Risk Level: ${riskLevel}`;
+
+//Getting the department of the risk on the card
+    const itemDepart = document.createElement("p");
+    itemDepart.textContent = department;
+
+
+   //Adopting the childs to item card
+    itemCard.appendChild(itemName);
+    itemCard.appendChild(itemLevel);
+    itemCard.appendChild(itemDepart);
+
+
+    riskContainer.appendChild(itemCard);
+}
+
+//Test Cases: 
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
+//addRiskItem("Server Down", "High", "IT");
+//addRiskItem("Someone spilled Coffee", "High", "Main Lounge");
  
+document.getElementById("submit-btn").addEventListener("click", function(event){
+//Getting all the input ids from the HTML Form
+    const inputName = document.getElementById("inputName");
+    const inputLvl = document.getElementById("inputLvl");
+    const inputDep = document.getElementById("inputDep");
+
+   //Calling the function to add new inputs
+    addRiskItem(inputName.value, inputLvl.value, inputDep.value);
+//clearing the form for additional inputs
+    inputName.value = "";
+    inputLvl.value = "";
+    inputDep.value = ";"
+    
+    
+
+})
+
